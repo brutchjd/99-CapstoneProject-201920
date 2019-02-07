e3477211ed840fa00c9d13af7f0fd76aeaa686d0
deckerdj <deckerdj@rose-hulman.edu>
2019-02-07 16:01:10 -0500

    changed go method

--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -67,8 +67,8 @@ def get_shared_frames(main_frame, mqtt_sender):
 
 def grid_frames(teleop_frame, arm_frame, control_frame):
     teleop_frame.grid(row=0, column=0)
-    arm_frame.grid(row=1, column=1)
-    control_frame.grid(row=2, column=2)
+    arm_frame.grid(row=1, column=0)
+    control_frame.grid(row=2, column=0)
 
 
 # -----------------------------------------------------------------------------
