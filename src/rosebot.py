b009de1bc5d55fd5412ee09c832d1d0c086fe597
craannj <craannj@rose-hulman.edu>
2019-02-12 15:52:10 -0500

    Proximity drive control

--- src/rosebot.py
+++ src/rosebot.py
@@ -173,6 +173,13 @@ class DriveSystem(object):
         Goes forward at the given speed until the robot is less than
         the given number of inches from the nearest object that it senses.
         """
+        # start = self.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+        self.go(speed, speed)
+        while True:
+            test = self.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            if test < inches:
+                break
+        self.stop()
 
     def go_backward_until_distance_is_greater_than(self, inches, speed):
         """
@@ -180,6 +187,12 @@ class DriveSystem(object):
         the given number of inches from the nearest object that it senses.
         Assumes that it senses an object when it starts.
         """
+        self.go(-1 * speed, -1 * speed)
+        while True:
+            test = self.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            if test > inches:
+                break
+        self.stop()
 
     def go_until_distance_is_within(self, delta, inches, speed):
         """
@@ -191,6 +204,24 @@ class DriveSystem(object):
         the robot should move until it is between 6.8 and 7.4 inches
         from the object.
         """
+        start = self.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+        if start > inches + delta:
+            self.go(speed, speed)
+            while True:
+                test = self.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+                if test > (inches - delta) and test < (inches + delta):
+                    break
+            self.stop()
+
+        if start < inches + delta:
+            self.go(-1 * speed, -1 * speed)
+            while True:
+                test = self.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+                if test > (inches - delta) and test < (inches + delta):
+                    break
+            self.stop()
+
+
 
     # -------------------------------------------------------------------------
     # Methods for driving that use the infrared beacon sensor.
