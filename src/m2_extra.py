b4e3ccd19e6d3a08bd95b7eded0f5f28b9b30343
craannj <craannj@rose-hulman.edu>
2019-02-13 16:53:00 -0500

    Proximity drive control

--- src/m2_extra.py
+++ src/m2_extra.py
@@ -4,5 +4,5 @@ import time
 
 def m2_pickup():
     robot = rosebot.RoseBot()
-    robot.drive_system.go_until_distance_is_within(2, 4, 50)
+    robot.drive_system.go_until_distance_is_within(0.9, 1, 25)
     robot.arm_and_claw.raise_arm()
