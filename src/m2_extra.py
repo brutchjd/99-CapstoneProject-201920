ab317e7db39e477f6e20cbc3fc696005a59bb2c1
craannj <craannj@rose-hulman.edu>
2019-02-14 16:25:58 -0500

    Proximity drive tones

--- src/m2_extra.py
+++ src/m2_extra.py
@@ -4,11 +4,45 @@ import time
 
 def m2_pickup():
     robot = rosebot.RoseBot()
-    robot.drive_system.go_until_distance_is_within(0.9, 1, 25)
-    while True:
-        distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
-        robot.sound_system.tone_maker.play_tone(150, 1000)
-        time.sleep(1 / distance)
-        if distance >= 1 - 1 or distance <= 1 + 1 :
-            break
+    go_until_distance_is_within_tone(robot, 1, 0, 25)
     robot.arm_and_claw.raise_arm()
+    # while True:
+       # distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+       # robot.sound_system.tone_maker.play_tone(150, 1000)
+       # time.sleep(1 / (distance + 0.00001))
+       # if distance >= 1 - 1 or distance <= 1 + 1:
+        #    break
+
+
+
+def go_until_distance_is_within_tone(robot, delta, inches, speed):
+    start = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+    print(start)
+
+    if start > inches + delta:
+        robot.drive_system.go(speed, speed)
+        while True:
+            test = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            robot.sound_system.tone_maker.play_tone(270 * (1 / (distance + 0.01)), 500)
+            time.sleep(1 / (distance + 0.00001))
+            print(test)
+            if test >= (inches - delta) and test <= (inches + delta):
+                print(test)
+                robot.drive_system.left_motor.turn_off()
+                robot.drive_system.right_motor.turn_off()
+                break
+
+    if start < inches + delta:
+        robot.drive_system.go(-1 * speed, -1 * speed)
+        while True:
+            test = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
+            robot.sound_system.tone_maker.play_tone(270 * (1 / (distance + 0.01)), 500)
+            time.sleep(1 / (distance + 0.00001))
+            print(test)
+            if test >= (inches - delta) and test <= (inches + delta):
+                print(test)
+                robot.drive_system.left_motor.turn_off()
+                robot.drive_system.right_motor.turn_off()
+                break
