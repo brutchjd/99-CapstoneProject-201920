679667ff11ad66bb654289cf1a39d15322d2f1b9
deckerdj <deckerdj@rose-hulman.edu>
2019-02-20 10:52:05 -0500

    added m3 sprint 3 file

--- src/m3_sprint_3.py
+++ src/m3_sprint_3.py
@@ -6,4 +6,12 @@ import rosebot
 import time
 import m3_run_this_on_laptop
 
-def
+def go_forward_until_scared(intensity, speed):
+    robot = rosebot.RoseBot()
+    robot.drive_system.go_straight_until_intensity_is_less_than(intensity, speed)
+    robot.sound_system.speech_maker('Ruh roh, Raggy')
+    robot.drive_system.go(speed, 0)
+    """Need a way to turn it 180 degrees"""
+
+
+
