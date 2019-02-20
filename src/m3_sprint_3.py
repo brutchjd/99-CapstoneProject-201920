0c93131714f54caf810b4f2d38a13236c4de8edc
deckerdj <deckerdj@rose-hulman.edu>
2019-02-20 16:57:03 -0500

    added scooby snack

--- src/m3_sprint_3.py
+++ src/m3_sprint_3.py
@@ -7,6 +7,8 @@ import time
 import m3_extra as cam
 
 def go_forward_until_scared(speed, intensity):
+    """Makes robot go forward until received intensity is less than given intensity.
+    The robot then speaks like Scooby, turns around, and runs away."""
     robot = rosebot.RoseBot()
     robot.drive_system.go_straight_until_intensity_is_less_than(intensity, speed)
     robot.sound_system.speech_maker.speak('Ruh roh, Raggy')
@@ -19,9 +21,10 @@ def go_forward_until_scared(speed, intensity):
 
 
 def scooby_snack(speed, area):
+    """The robot senses what a Scooby Snack looks like, and (with the given area) searches
+    for a Scooby Snack, makes a comment, then lifts (eats) the Scooby Snack."""
     robot = rosebot.RoseBot()
     cam.m3_camera_clockwise(speed, area)
-
     robot.sound_system.speech_maker.speak('Ooh, a Scooby Snack')
 
 
